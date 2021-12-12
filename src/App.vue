ec64c3007db284736508815151f62a909958cbec
Atticus Yong <86176575+AtticusYong@users.noreply.github.com>
2021-12-12 02:09:29 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
-
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResults">Reaction time: {{ score }} milliseconds</p>
+  <!-- <Results /> -->
 </template>
 
 <script>
-export default {
-  name: 'App',
-  components: {},
-}
+  import Block from './components/Block.vue';
+  
+  export default {
+    name: 'App',
+    components: { Block },
+    data() {
+      return {
+        isPlaying: false,
+        delay: null,
+        score: null,
+        showResults: false
+      }
+    },
+    methods: {
+      start() {
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+        showResults: false
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResults = true
+      }
+    }
+  }
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #444;
-    margin-top: 60px;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
 </style>
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,30 +1,48 @@
-<!-- <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    click me
+  </div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const state = reactive({ count: 0 })
+<script>
+  export default {
+    props: ['delay'],
+    data() {
+      return {
+        showBlock: false,
+        timer: null,
+        reactionTime: 0
+      }
+    },
+    mounted() {
+      // console.log('component mounted')
+      setTimeout(() => {
+        this.showBlock = true,
+        this.startTimer()
+      }, this.delay)
+    },
+    methods: {
+      startTimer() {
+        this.timer = setInterval(()=> {
+          this.reactionTime += 10
+        }, 10)
+      },
+      stopTimer() {
+        clearInterval(this.timer)
+        // console.log(reactionTime)
+        this.$emit('end', this.reactionTime)
+      }
+    }
+  }
 </script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style> -->
+<style>
+  .block{
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: #fff;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
